2c698e9916e60b694c4bfec04761ec8556c09fe2
Sanket Sugave <sanketsugave9881@gmail.com>
2025-07-28 15:06:09 +0530

    Fixed: connect to MongoDB Atlas

--- index.js
+++ index.js
@@ -15,7 +15,10 @@ const methodOverride = require('method-override');
 const app = express();
 
 
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 const db = mongoose.connection;
 db.on("error" , console.error.bind(console, "connection error"));
